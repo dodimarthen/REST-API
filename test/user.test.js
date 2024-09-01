ffbd3b4c134bea10ac17b8bc37e3b10bc56f2a7d
dodimarthen <dodimartin.sit@gmail.com>
2024-09-01 12:51:28 +0700

    adding negative test case

--- test/user.test.js
+++ test/user.test.js
@@ -6,7 +6,7 @@ describe("POST /api/users", function () {
   afterEach(async () => {
     await prismaCLient.user.deleteMany({
       where: {
-        username: "John",
+        username: "johndoe12",
       },
     });
   });
@@ -21,5 +21,15 @@ describe("POST /api/users", function () {
     expect(result.body.data.username).toBe("johndoe12");
     expect(result.body.data.name).toBe("John");
     expect(result.body.data.password).toBeUndefined();
+  });
+
+  it("should reject if request invalid", async () => {
+    const result = await supertest(web).post("/api/users").send({
+      username: "",
+      password: "",
+      name: "",
+    });
+    expect(result.status).toBe(400);
+    expect(result.body.errors).toBeDefined();
   });
 });
